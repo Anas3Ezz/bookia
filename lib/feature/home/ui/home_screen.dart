5614a9b065a2216f098c1e3d3af4b29c75bc128b
anas <anas33ezz@gmail.com>
2026-03-09 01:57:12 +0200

    refactor my screen

--- lib/feature/home/ui/home_screen.dart
+++ lib/feature/home/ui/home_screen.dart
@@ -1,6 +1,6 @@
 import 'package:bookia/feature/home/ui/widgets/best_seller_grid_view.dart';
 import 'package:bookia/feature/home/ui/widgets/book_carousel.dart';
-import 'package:bookia/gen/assets.gen.dart';
+import 'package:bookia/feature/home/ui/widgets/custom_app_bar.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:gap/gap.dart';
@@ -27,36 +27,6 @@ class HomeScreen extends StatelessWidget {
           BestSellerGridView(),
           const SliverToBoxAdapter(child: Gap(20)),
         ],
-      ),
-    );
-  }
-}
-
-class BookiaAppBar extends StatelessWidget implements PreferredSizeWidget {
-  const BookiaAppBar({super.key});
-
-  @override
-  Size get preferredSize => Size.fromHeight(56.h);
-
-  @override
-  Widget build(BuildContext context) {
-    return AppBar(
-      elevation: 0,
-      backgroundColor: Colors.white,
-      titleSpacing: 0,
-      title: Padding(
-        padding: EdgeInsets.symmetric(horizontal: 16.w),
-        child: Row(
-          children: [
-            Image.asset(Assets.images.appLogo.path, height: 28.h),
-            SizedBox(width: 8.w),
-            const Spacer(),
-            IconButton(
-              onPressed: () {},
-              icon: const Icon(size: 35, Icons.search, color: Colors.black),
-            ),
-          ],
-        ),
       ),
     );
   }
---
+++ lib/feature/home/ui/widgets/custom_app_bar.dart
@@ -0,0 +1,33 @@
+import 'package:bookia/gen/assets.gen.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+class BookiaAppBar extends StatelessWidget implements PreferredSizeWidget {
+  const BookiaAppBar({super.key});
+
+  @override
+  Size get preferredSize => Size.fromHeight(56.h);
+
+  @override
+  Widget build(BuildContext context) {
+    return AppBar(
+      elevation: 0,
+      backgroundColor: Colors.white,
+      titleSpacing: 0,
+      title: Padding(
+        padding: EdgeInsets.symmetric(horizontal: 16.w),
+        child: Row(
+          children: [
+            Image.asset(Assets.images.appLogo.path, height: 28.h),
+            SizedBox(width: 8.w),
+            const Spacer(),
+            IconButton(
+              onPressed: () {},
+              icon: const Icon(size: 35, Icons.search, color: Colors.black),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
