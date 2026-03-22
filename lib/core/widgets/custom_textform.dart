25099864bac8364cdebe2ec766484bda6fbbbc98
anas <anas33ezz@gmail.com>
2026-03-22 17:03:20 +0200

    refactor the textformfield

--- lib/core/widgets/custom_textform.dart
+++ lib/core/widgets/custom_textform.dart
@@ -1,15 +1,14 @@
 import 'package:flutter/material.dart';
 
-class CustomTextField extends StatelessWidget {
+class CustomTextField extends StatefulWidget {
   final String hintText;
   final bool isPassword;
   final Widget? suffixIcon;
   final TextEditingController controller;
-  // --- ADDED PROPERTIES ---
-  final String? Function(String?)? validator; // For form validation
-  final TextInputType? keyboardType; // To show the '@' or numeric keyboard
-  final Iterable<String>? autofillHints; // Helps password managers
-  final TextInputAction? textInputAction; // "Next" or "Done" button on keyboard
+  final String? Function(String?)? validator;
+  final TextInputType? keyboardType;
+  final Iterable<String>? autofillHints;
+  final TextInputAction? textInputAction;
 
   const CustomTextField({
     super.key,
@@ -24,41 +23,74 @@ class CustomTextField extends StatelessWidget {
   });
 
   @override
+  State<CustomTextField> createState() => _CustomTextFieldState();
+}
+
+class _CustomTextFieldState extends State<CustomTextField> {
+  final FocusNode _focusNode = FocusNode();
+  // Only start validating after the user has left the field at least once
+  bool _hasUnfocused = false;
+
+  @override
+  void initState() {
+    super.initState();
+    _focusNode.addListener(() {
+      if (!_focusNode.hasFocus && !_hasUnfocused) {
+        setState(() => _hasUnfocused = true);
+      }
+    });
+  }
+
+  @override
+  void dispose() {
+    _focusNode.dispose();
+    super.dispose();
+  }
+
+  static const _borderRadius = BorderRadius.all(Radius.circular(8));
+  static const _primaryColor = Color(0xFFBB9457);
+  static const _defaultBorderColor = Color(0xFFE8ECF4);
+
+  @override
   Widget build(BuildContext context) {
     return TextFormField(
-      autovalidateMode: AutovalidateMode.onUserInteraction,
-      controller: controller,
-      validator: validator, // Connects logic to the Form
-      keyboardType: keyboardType, // UX Improvement
-      autofillHints: autofillHints, // UX & Security Improvement
-      textInputAction: textInputAction, // Flow Improvement
-      obscureText: isPassword,
-      cursorColor: const Color(0xFFBB9457),
+      // Only auto-validate after the user has left the field once
+      autovalidateMode: _hasUnfocused
+          ? AutovalidateMode.onUserInteraction
+          : AutovalidateMode.disabled,
+      focusNode: _focusNode,
+      controller: widget.controller,
+      validator: widget.validator,
+      keyboardType: widget.keyboardType,
+      autofillHints: widget.autofillHints,
+      textInputAction: widget.textInputAction,
+      obscureText: widget.isPassword,
+      cursorColor: _primaryColor,
       decoration: InputDecoration(
-        focusedBorder: OutlineInputBorder(
-          borderRadius: BorderRadius.circular(8),
-          borderSide: const BorderSide(color: Color(0xFFBB9457), width: 1.5),
-        ),
-        hintText: hintText,
+        hintText: widget.hintText,
         hintStyle: const TextStyle(color: Color(0xFF8391A1)),
         fillColor: const Color(0xFFF7F8F9),
         filled: true,
-        suffixIcon: suffixIcon,
-        border: OutlineInputBorder(
-          borderRadius: BorderRadius.circular(8),
-          borderSide: const BorderSide(color: Color(0xFFE8ECF4)),
+        suffixIcon: widget.suffixIcon,
+        border: const OutlineInputBorder(
+          borderRadius: _borderRadius,
+          borderSide: BorderSide(color: _defaultBorderColor),
         ),
-        enabledBorder: OutlineInputBorder(
-          borderRadius: BorderRadius.circular(8),
-          borderSide: const BorderSide(color: Color(0xFFE8ECF4)),
+        enabledBorder: const OutlineInputBorder(
+          borderRadius: _borderRadius,
+          borderSide: BorderSide(color: _defaultBorderColor),
         ),
-        errorBorder: OutlineInputBorder(
-          borderRadius: BorderRadius.circular(8),
-          borderSide: const BorderSide(color: Colors.red, width: 1.5),
+        focusedBorder: const OutlineInputBorder(
+          borderRadius: _borderRadius,
+          borderSide: BorderSide(color: _primaryColor, width: 1.5),
         ),
-        focusedErrorBorder: OutlineInputBorder(
-          borderRadius: BorderRadius.circular(8),
-          borderSide: const BorderSide(color: Colors.red, width: 1.5),
+        errorBorder: const OutlineInputBorder(
+          borderRadius: _borderRadius,
+          borderSide: BorderSide(color: Colors.red, width: 1.5),
+        ),
+        focusedErrorBorder: const OutlineInputBorder(
+          borderRadius: _borderRadius,
+          borderSide: BorderSide(color: Colors.red, width: 1.5),
         ),
       ),
     );
